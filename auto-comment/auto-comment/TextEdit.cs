753d7e59ccbdcf18b4a747fe9c9122ed8631b0c4
IvailoEKSDI <41078003+IvailoEKSDI@users.noreply.github.com>
2019-02-08 22:41:05 +0200

    TextEdit class
    TextEdit.Split(string name);

---
+++ auto-comment/auto-comment/TextEdit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auto_comment
+{
+    class TextEdit
+    {
+        public static string[] split(string wholetext)
+        {
+            wholetext = wholetext.Remove('\t');
+            string[] splittext = wholetext.Split('\n');
+            return splittext;
+        }
+    }
+}
