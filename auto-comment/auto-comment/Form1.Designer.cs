c505a3919ef6c36b245dcf2b451c81ec6862978c
kufte <kuftebg101@gmail.com>
2019-02-08 22:47:05 +0200

    ree

--- auto-comment/auto-comment/Form1.Designer.cs
+++ auto-comment/auto-comment/Form1.Designer.cs
@@ -28,17 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.com = new System.Windows.Forms.Button();
+            this.btn_comment = new System.Windows.Forms.Button();
+            this.btn_open = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // com
+            // btn_comment
             // 
-            this.com.Location = new System.Drawing.Point(179, 39);
-            this.com.Name = "com";
-            this.com.Size = new System.Drawing.Size(75, 23);
-            this.com.TabIndex = 0;
-            this.com.Text = "Comment";
-            this.com.UseVisualStyleBackColor = true;
+            this.btn_comment.Location = new System.Drawing.Point(116, 12);
+            this.btn_comment.Name = "btn_comment";
+            this.btn_comment.Size = new System.Drawing.Size(213, 139);
+            this.btn_comment.TabIndex = 0;
+            this.btn_comment.Text = "Comment";
+            this.btn_comment.UseVisualStyleBackColor = true;
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(116, 189);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(213, 139);
+            this.btn_open.TabIndex = 1;
+            this.btn_open.Text = "open";
+            this.btn_open.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -46,7 +56,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(464, 367);
-            this.Controls.Add(this.com);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.btn_comment);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +66,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button com;
+        private System.Windows.Forms.Button btn_comment;
+        private System.Windows.Forms.Button btn_open;
     }
 }
 
