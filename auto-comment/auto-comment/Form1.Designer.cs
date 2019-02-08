342caeb07e42cefa36c08148636df7abd6fe9f7b
kufte <kuftebg101@gmail.com>
2019-02-08 22:43:50 +0200

    fuck

--- auto-comment/auto-comment/Form1.Designer.cs
+++ auto-comment/auto-comment/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.com = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // com
+            // 
+            this.com.Location = new System.Drawing.Point(179, 39);
+            this.com.Name = "com";
+            this.com.Size = new System.Drawing.Size(75, 23);
+            this.com.TabIndex = 0;
+            this.com.Text = "Comment";
+            this.com.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(464, 367);
+            this.Controls.Add(this.com);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button com;
     }
 }
 
