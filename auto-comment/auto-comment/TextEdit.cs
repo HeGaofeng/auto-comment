32a2451e4969f7972592e66fa9d7742dc0dae519
IvailoEKSDI <41078003+IvailoEKSDI@users.noreply.github.com>
2019-02-08 23:08:35 +0200

    Shit is working now

--- auto-comment/auto-comment/TextEdit.cs
+++ auto-comment/auto-comment/TextEdit.cs
@@ -10,7 +10,7 @@ namespace auto_comment
     {
         public static string[] split(string wholetext)
         {
-            wholetext = wholetext.Remove('\t'); //premahva vsichki tabove ot texta
+            wholetext = wholetext.Trim('\t'); //premahva vsichki tabove ot texta
             string[] splittext = wholetext.Split('\n'); //vrushta array ot stringove kato vseki ot tozi string suvpada s redovete
             return splittext; //just to push smth
         }
