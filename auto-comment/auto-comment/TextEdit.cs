1ccc647951ebb547246283940f652af7c5805556
IvailoEKSDI <41078003+IvailoEKSDI@users.noreply.github.com>
2019-02-08 23:17:42 +0200

    Added SpecifilLine returning
    Provide the text and a line and it will return that line

--- auto-comment/auto-comment/TextEdit.cs
+++ auto-comment/auto-comment/TextEdit.cs
@@ -8,11 +8,16 @@ namespace auto_comment
 {
     class TextEdit
     {
-        public static string[] split(string wholetext)
+        public static string[] Split(string wholetext)
         {
             wholetext = wholetext.Trim('\t'); //premahva vsichki tabove ot texta
             string[] splittext = wholetext.Split('\n'); //vrushta array ot stringove kato vseki ot tozi string suvpada s redovete
             return splittext; //just to push smth
+        }
+        public static string SpecificLine(string wholetext, int index)
+        {
+            string[] splittext = Split(wholetext);
+            return splittext[index];
         }
     }
 }
