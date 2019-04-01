64afe1f9fec3c07ddd44f3e78ee90033c491f60f
IvailoEKSDI <41078003+IvailoEKSDI@users.noreply.github.com>
2019-04-01 20:36:55 +0300

    design change

--- auto-comment/auto-comment/Auto-Comment.Designer.cs
+++ auto-comment/auto-comment/Auto-Comment.Designer.cs
@@ -31,7 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(auto_comment));
             this.btn_comment = new System.Windows.Forms.Button();
             this.btn_open = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.btn_twitter = new System.Windows.Forms.Button();
             this.btn_options = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -42,11 +41,19 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_comment.BackColor = System.Drawing.Color.Transparent;
-            this.btn_comment.BackgroundImage = global::auto_comment.Properties.Resources.icons8_note_512;
             this.btn_comment.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_comment.Location = new System.Drawing.Point(345, 77);
+            this.btn_comment.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.btn_comment.FlatAppearance.BorderSize = 0;
+            this.btn_comment.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.btn_comment.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_comment.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_comment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_comment.Location = new System.Drawing.Point(714, 229);
+            this.btn_comment.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_comment.MaximumSize = new System.Drawing.Size(482, 388);
+            this.btn_comment.MinimumSize = new System.Drawing.Size(241, 194);
             this.btn_comment.Name = "btn_comment";
-            this.btn_comment.Size = new System.Drawing.Size(207, 207);
+            this.btn_comment.Size = new System.Drawing.Size(349, 279);
             this.btn_comment.TabIndex = 0;
             this.btn_comment.UseVisualStyleBackColor = false;
             this.btn_comment.Click += new System.EventHandler(this.btn_comment_Click);
@@ -57,49 +64,62 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_open.BackColor = System.Drawing.Color.Transparent;
-            this.btn_open.BackgroundImage = global::auto_comment.Properties.Resources.OpenCode;
             this.btn_open.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_open.Location = new System.Drawing.Point(45, 77);
+            this.btn_open.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.btn_open.FlatAppearance.BorderSize = 0;
+            this.btn_open.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.btn_open.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_open.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_open.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_open.Location = new System.Drawing.Point(222, 229);
+            this.btn_open.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_open.MaximumSize = new System.Drawing.Size(482, 388);
+            this.btn_open.MinimumSize = new System.Drawing.Size(241, 194);
             this.btn_open.Name = "btn_open";
-            this.btn_open.Size = new System.Drawing.Size(207, 207);
+            this.btn_open.Size = new System.Drawing.Size(323, 279);
             this.btn_open.TabIndex = 1;
             this.btn_open.UseVisualStyleBackColor = false;
             this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
             // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(178, -2);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(237, 47);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Auto-Comment";
-            // 
             // btn_twitter
             // 
-            this.btn_twitter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_twitter.BackgroundImage = global::auto_comment.Properties.Resources.twitter_logo_2;
+            this.btn_twitter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_twitter.BackColor = System.Drawing.Color.Transparent;
             this.btn_twitter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_twitter.Location = new System.Drawing.Point(12, 289);
+            this.btn_twitter.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.btn_twitter.FlatAppearance.BorderSize = 0;
+            this.btn_twitter.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.btn_twitter.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_twitter.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_twitter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_twitter.Location = new System.Drawing.Point(23, 512);
+            this.btn_twitter.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_twitter.MaximumSize = new System.Drawing.Size(284, 208);
+            this.btn_twitter.MinimumSize = new System.Drawing.Size(142, 104);
             this.btn_twitter.Name = "btn_twitter";
-            this.btn_twitter.Size = new System.Drawing.Size(50, 50);
+            this.btn_twitter.Size = new System.Drawing.Size(205, 146);
             this.btn_twitter.TabIndex = 4;
             this.btn_twitter.TextAlign = System.Drawing.ContentAlignment.BottomRight;
-            this.btn_twitter.UseVisualStyleBackColor = true;
+            this.btn_twitter.UseVisualStyleBackColor = false;
             this.btn_twitter.Click += new System.EventHandler(this.btn_twitter_Click);
             // 
             // btn_options
             // 
-            this.btn_options.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.btn_options.Location = new System.Drawing.Point(477, 317);
+            this.btn_options.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_options.BackColor = System.Drawing.Color.Transparent;
+            this.btn_options.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.btn_options.FlatAppearance.BorderSize = 0;
+            this.btn_options.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.btn_options.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_options.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_options.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_options.Location = new System.Drawing.Point(1031, 565);
+            this.btn_options.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_options.MaximumSize = new System.Drawing.Size(292, 122);
+            this.btn_options.MinimumSize = new System.Drawing.Size(146, 61);
             this.btn_options.Name = "btn_options";
-            this.btn_options.Size = new System.Drawing.Size(75, 23);
+            this.btn_options.Size = new System.Drawing.Size(222, 93);
             this.btn_options.TabIndex = 5;
-            this.btn_options.Text = "Options";
             this.btn_options.UseVisualStyleBackColor = false;
             this.btn_options.Click += new System.EventHandler(this.btn_options_Click);
             // 
@@ -107,24 +127,24 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.BackgroundImage = global::auto_comment.Properties.Resources.backgrondcode;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.BackgroundImage = global::auto_comment.Properties.Resources.background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(584, 357);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.btn_options);
             this.Controls.Add(this.btn_twitter);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.btn_open);
             this.Controls.Add(this.btn_comment);
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.Color.Transparent;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(600, 396);
-            this.MinimumSize = new System.Drawing.Size(600, 396);
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
             this.Name = "auto_comment";
             this.Text = "Auto-Comment";
             this.ResumeLayout(false);
-            this.PerformLayout();
+            this.MaximizeBox = false;
+            // Set the MinimizeBox to false to remove the minimize box.
 
         }
 
@@ -132,7 +152,6 @@
 
         private System.Windows.Forms.Button btn_comment;
         private System.Windows.Forms.Button btn_open;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btn_twitter;
         private System.Windows.Forms.Button btn_options;
     }
